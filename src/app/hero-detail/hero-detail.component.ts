f1bdc538831490a4972b93fba126941acd672376
ava8tor <mikeraymond211@gmail.com>
2024-03-14 15:25:45 -0400

    Configured routing for navigation, added dashboard, and moved details component to its own page

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';;
-import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';;
+import { CommonModule, Location } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 import { Hero } from '../hero'
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-hero-detail',
@@ -11,8 +13,43 @@ import { Hero } from '../hero'
   styleUrl: './hero-detail.component.css'
 })
 
-export class HeroDetailComponent {
+export class HeroDetailComponent implements OnInit {
   
   @Input() hero?: Hero;
+
+  /*ActivatedRoute holds information about the route to this 
+    instance of the HeroDetailComponent
+  HeroService gets hero data from the remote server and this 
+    component uses it to get the hero-to-display.
+  location is an Angular service for interacting with the 
+    browser. This service lets you navigate back to the previous 
+    view.
+  */
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
+
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  getHero(): void {
+
+    /*route.snapshot is a static image of the route 
+        information shortly after the component was created.
+      paramMap is a dictionary of route parameter values 
+        extracted from the URL. The "id" key returns the id 
+        of the hero to fetch.
+    */
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { Hero } from '../hero';
 // import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
@@ -10,11 +11,12 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, HeroDetailComponent],
+  imports: [CommonModule, FormsModule, RouterModule, 
+    HeroDetailComponent],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit{
   
   // hero: Hero = {
   //   id: 1,
@@ -22,9 +24,10 @@ export class HeroesComponent {
   // };
 
   // heroes = HEROES;
-  heroes: Hero[] = [];
   
-  selectedHero?: Hero;
+  // selectedHero?: Hero;
+
+  heroes: Hero[] = [];
 
 
   constructor(private heroService: HeroService, private messageService: MessageService){}
@@ -39,9 +42,9 @@ export class HeroesComponent {
       .subscribe(heroes => this.heroes = heroes);
   }
 
-  onSelect(hero: Hero): void{
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
+  // onSelect(hero: Hero): void{
+  //   this.selectedHero = hero;
+  //   this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  // }
 
 }
