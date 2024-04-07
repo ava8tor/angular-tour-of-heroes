3ad362a6d7ff16a1c91c173c0086d9fe1fa4d712
ava8tor <ava8tor@gmail.com>
2024-04-07 12:29:57 -0400

    Added http methods

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -36,11 +36,26 @@ export class HeroesComponent implements OnInit{
     this.getHeroes();
   }
 
+  add(name: string): void {
+    name = name.trim();
+    if(!name) { return; }
+    this.heroService.addHero({name} as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      })
+  }
+
   getHeroes(): void {
     // this.heroes = this.heroService.getHeroes();
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
+
+  delete(hero:Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
 
   // onSelect(hero: Hero): void{
   //   this.selectedHero = hero;
